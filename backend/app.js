acd79816c0185aff65be0740ade721d4d8124d2f
Elena Yadryshnikova <elenaya014@gmail.com>
2023-07-18 19:45:11 +0700

    add crash test

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(corsResolver);
 app.use(requestLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', authRouter);
 app.use('/', authVerifier);
 app.use('/', usersRouter);
