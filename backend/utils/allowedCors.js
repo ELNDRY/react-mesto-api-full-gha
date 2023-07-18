2a6254a689793ba96500f0c7b37eef9968dcf51d
Elena Yadryshnikova <elenaya014@gmail.com>
2023-07-18 22:07:35 +0700

    add allowed cors

--- backend/utils/allowedCors.js
+++ backend/utils/allowedCors.js
@@ -1,6 +1,10 @@
 const allowedCors = [
   'http://localhost:3000',
   'https://localhost:3000',
+  'http://elndry.students.nomoredomains.xyz',
+  'https://elndry.students.nomoredomains.xyz',
+  'http://api.elndry.students.nomoredomains.xyz',
+  'https://api.elndry.students.nomoredomains.xyz',
 ];
 
 module.exports = allowedCors;
